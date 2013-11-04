6f2971ce022d066f3b00c6fe652022729eda81f2
Brendan Reddy-Best <b.d.reddybest@gmail.com>
2013-11-04 01:27:23 -0800

    Update compass.ino

--- compass.ino
+++ compass.ino
@@ -13,19 +13,31 @@ void setup() {
   Wire.write(byte(0x00));
   Wire.endTransmission();
 }
-int compass() {
+void loop(){Serial.println(compass());}
+
+float compass() {
   int x, y, z;
 
   // Initiate communications with compass
-  Wire.beginTransmission(Addr);
-  Wire.write(byte(0x03));       // most significant byte first
+  Wire.beginTransmission(compass);
+  Wire.write(byte(0x03));       // Send request to X MSB register
   Wire.endTransmission();
 
-  Wire.requestFrom(Addr, 6);    // request two bytes per axis
-  if(Wire.available() <=6) {    // need  6 bytes available
+  Wire.requestFrom(compass, 6);    // Request 6 bytes; 2 bytes per axis
+  if(Wire.available() <=6) {    // If 6 bytes available
     x = Wire.read() << 8 | Wire.read();
     z = Wire.read() << 8 | Wire.read();
     y = Wire.read() << 8 | Wire.read();
+
   }
-return(x,y,z);  
+   // Calculate heading when the magnetometer is level, then correct for signs of axis.
+  float heading = atan2(x, y);
+   
+  // Correct for when signs are reversed.
+  /*if(heading < 0)
+    heading += 2*PI;
+   
+  // Convert radians to degrees for readability.
+  float h_deg = heading * 180/M_PI; 
+  */return (heading);
 }
