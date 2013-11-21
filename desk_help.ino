a8a7e5bb79a9a66d6901ec1b02397aea16b038f6
Brendan Reddy-Best <b.d.reddybest@gmail.com>
2013-11-21 14:57:39 -0800

    Create desk_help.ino
    controls the robot arm on my workbench with potentiometers

---
+++ desk_help.ino
@@ -0,0 +1,32 @@
+#include <Servo.h>
+
+Servo claw, tilt, pan;
+
+char cmd_byte;
+int potpin1 (A0);
+int potpin2 (A1);
+int potpin3 (A2);
+
+void setup(){
+  Serial.begin(9600);
+  claw.attach(8);
+  tilt.attach(7);
+  pan.attach(6);
+}
+  
+void loop(){
+  int val1 = analogRead(potpin1);      
+  int val2 = analogRead(potpin2);            
+  int val3 = analogRead(potpin3);        
+
+  val1 = map(val1, 0, 1023, 0, 179);     
+ 
+  claw.write(val1);  
+  delay(50);
+  val2 = map(val2, 0, 1023, 0, 179);   
+  tilt.write(val2);                   
+  delay(50);
+  val3 = map(val3, 0, 1023, 0, 179); 
+  pan.write(val3);                   
+  delay(50);                          
+}
